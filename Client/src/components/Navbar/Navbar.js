ff077a07b96e316ec1a373bdfc95fce30a92014b
khalid saaf <khallid.saaf@gmail.com>
2021-05-11 12:34:08 +0000

    mis a jour

--- Client/src/components/Navbar/Navbar.js
+++ Client/src/components/Navbar/Navbar.js
@@ -1,28 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Navbar'
 
 const Navbar = () => {
   return (
     <div>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light navblog">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">Country Api</a>
+        <a className="navbar-brand" href="/">Country Api React</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/"><Link to="/capital">capitales des pays</Link> </a>
+              <a className="nav-link active" aria-current="page" href="/"><Link to="/capital">Capitales des pays</Link> </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/"><Link to="/codes">codes de pays</Link></a>
+              <a className="nav-link" href="/"><Link to="/codes">Codes de pays</Link></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/"><Link to="/langues">les langues</Link></a>
+              <a className="nav-link" href="/"><Link to="/langues">Langues</Link></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/"><Link to="/paysN">nom du pays</Link></a>
+              <a className="nav-link" href="/"><Link to="/paysN">Nom de paye</Link></a>
             </li>
           </ul>
     
